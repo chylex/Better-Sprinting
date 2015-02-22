6903b4812c89040ea43b8debac13cba121637284
chylex <chylex@fo2.cz>
2015-02-22 10:58:11 +0100

    Pushed 1.1.1 update

--- src/main/java/chylex/bettersprinting/BetterSprintingMod.java
+++ src/main/java/chylex/bettersprinting/BetterSprintingMod.java
@@ -18,7 +18,7 @@ public class BetterSprintingMod{
 	
 	public static BetterSprintingConfig config;
 	
-	public static final String buildId = "30-01-2015-0";
+	public static final String buildId = "22-02-2015-0";
 	public static String modVersion;
 	
 	@EventHandler
