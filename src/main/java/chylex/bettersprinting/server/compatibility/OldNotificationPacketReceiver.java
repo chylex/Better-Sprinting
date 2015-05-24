6980359dc4013f2bdb54eae40a0fd3136635da76
chylex <info@chylex.com>
2015-05-24 18:48:38 +0200

    Update version, fix AT and old notification packet receiver

--- src/main/java/chylex/bettersprinting/server/compatibility/OldNotificationPacketReceiver.java
+++ src/main/java/chylex/bettersprinting/server/compatibility/OldNotificationPacketReceiver.java
@@ -1,5 +1,4 @@
 package chylex.bettersprinting.server.compatibility;
-import gnu.trove.map.hash.TObjectIntHashMap;
 import io.netty.buffer.ByteBuf;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,8 +10,6 @@ import chylex.bettersprinting.server.ServerSettings;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.Phase;
-import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
 import cpw.mods.fml.common.network.FMLEventChannel;
 import cpw.mods.fml.common.network.FMLNetworkEvent.ServerCustomPacketEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
@@ -28,7 +25,7 @@ public class OldNotificationPacketReceiver{
 	public static void kickOldModUsers(){
 		ServerConfigurationManager manager = MinecraftServer.getServer().getConfigurationManager();
 		
-		for(String id:instance.users.keySet()){
+		for(String id:instance.users){
 			EntityPlayerMP player = manager.func_152612_a(id);
 			if (player != null)player.playerNetServerHandler.kickPlayerFromServer("The server does not allow Better Sprinting. Newer versions of the mod can be disabled automatically without kicking.");
 		}
@@ -37,7 +34,7 @@ public class OldNotificationPacketReceiver{
 	}
 	
 	private final FMLEventChannel channel;
-	private final TObjectIntHashMap<String> users = new TObjectIntHashMap<>();
+	private final Set<String> users = new HashSet<>();
 	
 	private OldNotificationPacketReceiver(){
 		channel = NetworkRegistry.INSTANCE.newEventDrivenChannel("BSprint");
@@ -49,35 +46,10 @@ public class OldNotificationPacketReceiver{
 		ByteBuf data = e.packet.payload();
 		
 		if (data.readByte() == 4){
-			NetHandlerPlayServer net = (NetHandlerPlayServer)e.handler;
-			users.put(net.playerEntity.getCommandSenderName(),100);
-		}
-	}
-	
-	@SubscribeEvent
-	public void onServerTick(ServerTickEvent e){
-		if (e.phase == Phase.END || !ServerSettings.disableClientMod)return;
-		
-		Set<String> ids = new HashSet<>(users.keySet());
-		
-		for(String id:ids){
-			int val = users.adjustOrPutValue(id,-1,0);
+			NetHandlerPlayServer handler = (NetHandlerPlayServer)e.handler;
 			
-			EntityPlayerMP player = MinecraftServer.getServer().getConfigurationManager().func_152612_a(id);
-			
-			if (player == null){
-				users.remove(id);
-				continue;
-			}
-			
-			if (val > 10 && val < 80){
-				if (player.playerNetServerHandler.netManager.channel().isWritable())users.put(id,val = 10);
-			}
-			
-			if (val <= 0){
-				users.remove(id);
-				player.playerNetServerHandler.kickPlayerFromServer("The server does not allow Better Sprinting. Newer versions of the mod can be disabled automatically without kicking.");
-			}
+			if (ServerSettings.disableClientMod)handler.kickPlayerFromServer("The server does not allow Better Sprinting. Newer versions of the mod can be disabled automatically without kicking.");
+			else users.add(handler.playerEntity.getCommandSenderName());
 		}
 	}
 	
