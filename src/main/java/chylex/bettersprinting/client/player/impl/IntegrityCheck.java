b29089d27ba277c0d3cb1253541d640591bd75ad
chylex <contact@chylex.com>
2017-06-15 08:13:16 +0200

    Rewrite integrity check

--- src/main/java/chylex/bettersprinting/client/player/impl/IntegrityCheck.java
+++ src/main/java/chylex/bettersprinting/client/player/impl/IntegrityCheck.java
@@ -1,8 +1,6 @@
 package chylex.bettersprinting.client.player.impl;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -21,34 +19,21 @@ public final class IntegrityCheck{
 	
 	public static void unregister(){
 		MinecraftForge.EVENT_BUS.unregister(instance);
-		instance.initialPlayerPos = Vec3d.ZERO;
 	}
 
 	private static final IntegrityCheck instance = new IntegrityCheck();
 	private static final Minecraft mc = Minecraft.getMinecraft();
 	
-	private Vec3d initialPlayerPos = Vec3d.ZERO;
-	
 	private IntegrityCheck(){}
 	
 	@SubscribeEvent
 	public void onPlayerTick(ClientTickEvent e){
-		if (e.phase == Phase.END && mc.player != null && mc.playerController != null){
-			if (LivingUpdate.checkIntegrity()){
-				unregister();
-			}
-			else if (initialPlayerPos.equals(Vec3d.ZERO)){
-				initialPlayerPos = getPlayerPosXZ(mc.player);
-			}
-			else if (getPlayerPosXZ(mc.player).squareDistanceTo(initialPlayerPos) > 1D){
+		if (e.phase == Phase.END && mc.player != null && mc.player.ticksExisted > 1){
+			if (!LivingUpdate.checkIntegrity()){
 				mc.player.sendMessage(new TextComponentString(ClientModManager.chatPrefix+I18n.format("bs.game.integrity")));
-				unregister();
 			}
+			
+			unregister();
 		}
-	}
-	
-	private static Vec3d getPlayerPosXZ(EntityPlayer player){
-		Vec3d vec = player.getPositionVector();
-		return new Vec3d(vec.x, 0D, vec.z);
 	}
 }
