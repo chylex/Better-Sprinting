88542761c9a3822782468c9bbc73946ccbc4f000
chylex <contact@chylex.com>
2019-03-02 17:40:24 +0100

    Fix LAN crashing & enable sprinting in all directions if LAN owner has the mod

--- src/main/java/chylex/bettersprinting/client/ClientNetwork.java
+++ src/main/java/chylex/bettersprinting/client/ClientNetwork.java
@@ -15,8 +15,19 @@ public class ClientNetwork implements INetworkHandler{
 		return buffer;
 	}
 	
+	public static PacketBuffer writeLanSettings(){
+		PacketBuffer buffer = PacketPipeline.buf();
+		buffer.writeByte(0).writeBoolean(true).writeBoolean(true);
+		return buffer;
+	}
+	
 	@Override
 	public void onPacket(Side side, ByteBuf data, EntityPlayer player){
+		if (side == Side.SERVER){
+			PacketPipeline.sendToPlayer(writeLanSettings(), player);
+			return;
+		}
+		
 		byte type = data.readByte();
 		
 		if (type == 0){
